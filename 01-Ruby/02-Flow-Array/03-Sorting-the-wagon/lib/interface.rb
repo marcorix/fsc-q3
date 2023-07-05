de722410e7bc3ef480338c570ea5272ba80cb92b
Marco Ricci <mr.ricci79@gmail.com>
2023-07-05 15:58:23 +0200

    sorted students

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -4,8 +4,25 @@ require_relative "wagon_sort"
 #       Remember, to read an input from the command line, use:
 #       - `gets`:  https://ruby-doc.org/core-2.7.5/Kernel.html#method-i-gets
 #       - `chomp`: https://ruby-doc.org/core-2.7.5/String.html#method-i-chomp
+students = []
 
+puts "Type a student name:"
+name = gets.chomp
+students << name
 
-
+while name != ""
+  puts "Type another student name or press enter to finish:"
+  name = gets.chomp
+  students << name if name != ""
+end
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+sorted_students = wagon_sort(students)
+
+if sorted_students.size >= 2
+  puts "Congratulations! Your Wagon has #{students.size} students:"
+  puts "#{sorted_students[0...-1].join(', ')} and #{sorted_students.last}"
+else
+  puts "Congratulations! Your Wagon has #{students.size} student:"
+  puts sorted_students.first
+end
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,7 @@
 def wagon_sort(students)
   # TODO: return (not print!) a copy of students, sorted alphabetically
+  students.sort_by {|name| name.upcase }
 end
+
+
+# p wagon_sort(["giorgio", "Luca", "anna"])
